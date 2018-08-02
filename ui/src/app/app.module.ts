30596ab34ae0ea74871065548f87d0e0d806ffac
Prakash Pandey <pcpandey@mail.com>
2018-08-03 02:02:53 +0530

    Added angular ui component

---
+++ ui/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { CreateComponent } from './create/create.component';
+import { MoviesComponent } from './movies/movies.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateComponent,
+    MoviesComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
