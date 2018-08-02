03e66fa74ac0cb7f57acef5799d5a5730bada2ff
Prakash Pandey <pcpandey@mail.com>
2018-08-03 02:58:55 +0530

    work in progress

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { RouterModule } from '@angular/router';
+import { AvailableRoutes } from './app.routing';
+
 import { AppComponent } from './app.component';
 import { CreateComponent } from './create/create.component';
 import { MoviesComponent } from './movies/movies.component';
@@ -16,7 +19,9 @@ import { MoviesComponent } from './movies/movies.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule,
+    RouterModule.forRoot(AvailableRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
